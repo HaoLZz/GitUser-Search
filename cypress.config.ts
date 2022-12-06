f15c3e4e9db2b58ca018fdd593066383638e48b1
Haoliang Zhang <hlzhang26@outlook.com>
2022-12-06 08:56:14 -0700

    set up Cypress for e2e and component testing

---
+++ cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+    specPattern: [
+      "**/**/*.comp-spec.{js,ts,jsx,tsx}",
+      "**/**/*.cy.{js,ts,jsx,tsx}",
+    ],
+  },
+});
